a9ab62c25e0c9a99864ef8a29ee69d8367f40eb4
Illuminaxx <nexgen62@hotmail.fr>
2019-07-30 15:47:19 +0200

    fix yarn

---
+++ src/TodoList.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+
+class TodoList extends Component {
+    componentDidUpdate() {
+        this.props.inputElement.current.focus()
+    }
+    render() {
+        return (
+            <div className="todoListMain">
+                <div className="header">
+                    <h1 className="title">TODO LIST</h1>
+                    <form onSubmit={this.props.addItem}>
+                        <input
+                            placeholder="Tâches"
+                            ref={this.props.inputElement}
+                            value={this.props.currentItem.text}
+                            onChange={this.props.handleInput}
+                        />
+                        <button className="button  button4" type="submit"> Ajouter </button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TodoList
